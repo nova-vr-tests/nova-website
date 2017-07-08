82caf5363113422c57ace440f4e17b5989d00f61
0648596375 <tbinetruy@gmail.com>
2017-07-08 20:00:41 +0200

    quick fix

--- novawebsite/settings.py
+++ novawebsite/settings.py
@@ -75,18 +75,19 @@ WSGI_APPLICATION = 'novawebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 if DEBUG == False:
     # Update database configuration with $DATABASE_URL.
     import dj_database_url
     db_from_env = dj_database_url.config()
     DATABASES['default'].update(db_from_env)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
