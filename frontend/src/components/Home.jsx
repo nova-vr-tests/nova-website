c54952dc38ce75ff4c7692c0c4f3e7a9b8602acb
0648596375 <tbinetruy@gmail.com>
2017-07-09 23:00:26 +0200

    created homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect }from 'react-redux';
 import { push } from 'react-router-redux';
 import './Home.css';
+import decoration from './img/home/decoration1.jpg';
 
 const reduxStatePropTypes = {
 }
@@ -23,7 +24,29 @@ const mapDispatchToProps = function(dispatch) {
 
 const HomeDumb = props => (
   <div className="home--wrapper">
-    <h1>Coming Soon</h1>
+    <div className="text--wrapper">
+      <h1>New Year's Eve in VR</h1>
+      <h2>Anywhere Anytime</h2>
+      <div className="text--content">
+        <p>
+          Live from around the world, in stereoscopic 3D virtual reality,
+          we will bring you to 10 of the most iconic locations on Earth to
+          celebrate with locals.
+        </p>
+        <p>
+          All you need is the internet.
+        </p>
+        <p>
+          Imagine jumping from one city to another with the click of your controller. This New Year's Eve you can travel the globe without ever leaving your living room. Teleport, fly, or transform into a mega-robot with friends and strangers alike.
+        </p>
+        <p>
+          This year will be like none other !
+        </p>
+      </div>
+    </div>
+    <div className="image--wrapper">
+      <img src={ decoration } className="supporting-image" alt="decoration" />
+    </div>
   </div>
 )
 
