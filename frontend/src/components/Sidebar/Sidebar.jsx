a0afe9ddc1280384e433fe695211e771e1b935fa
Thomas Binetruy <tbinetruy@gmail.com>
2017-09-17 13:46:07 -0400

    finished sidebar dyncamics

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -10,10 +10,81 @@ import {
     toggleSidebarSubSection
 } from '../../reducer/actions/Sidebar.js'
 
+const constants = {
+    styles: {
+        unitHeight: 100/24 + 'vh',
+        unitWidth: '60px',
+        sidebar: {
+            widthFactor: 3,
+        },
+    },
+}
+
 
 const SidebarSubSection = props => {
     const { subSection } = props
     const subSubSections = subSection.links
+
+    const unitHeight = constants.styles.unitHeight
+
+    const sidebarWidth =  constants.styles.sidebar.widthFactor + ' * ' + constants.styles.unitWidth
+
+    const styles = {
+        subSection:{
+            wrapper: {
+                display: 'flex',
+                flexDirection: 'row',
+                minHeight: unitHeight,
+                overflowY: 'hidden',
+                transition: 'height 0.2s linear',
+                overflow: 'visible',
+            },
+            opened: {
+                height: unitHeight,
+            },
+            title: {
+                minWidth: 'calc(' + sidebarWidth + ')',
+                minHeight: unitHeight,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                flex: 1,
+            },
+            link: {
+                minHeight: unitHeight,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+            },
+        },
+        subSubSection:{
+            wrapper: {
+                display: 'flex',
+                flexDirection: 'column',
+                left: '100%',
+                right: '-100%',
+                top: 0,
+                opacity: 0,
+                pointerEvents: 'none',
+                transition: 'opacity 0.2s linear',
+                position: 'absolute',
+            },
+            opened: {
+                opacity: 1,
+                pointerEvents: 'inherit',
+            },
+            link: {
+                minHeight: unitHeight,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(255, 255, 255, 0.05)',
+                width: 'calc(' + sidebarWidth + ')',
+            },
+        },
+    }
 
     if(subSubSections.length > 0) {
         const components = []
@@ -22,27 +93,27 @@ const SidebarSubSection = props => {
         for(let i = 0; i < subSubSections.length; i++) {
             const subSubSection = subSubSections[i]
             components[i] = <div
-                                className="sub-sub-section--link"
+                                style={ styles.subSubSection.link }
                                 key={ i }>
                                 { subSubSection }
                             </div>
         }
 
         return (
-            <div className={ "sub-section--wrapper" + (props.isOpened ? " opened" : "") }>
-                <div
-                    className="sub-section--title"
-                    onClick={ () => props.dispatch.toggleSidebarSubSection(props.id.section, props.id.subSection) }>
-                    { subSection.title }
-                </div>
-                <div className="sub-sub-sections--wrapper">
-                    { components }
-                </div>
+            <div style={ {...styles.subSection.wrapper, ...(props.isOpened ? styles.subSection.opened : {})} }>
+                    <div
+                        style={ styles.subSection.title }
+                        onClick={ () => props.dispatch.toggleSidebarSubSection(props.id.section, props.id.subSection) }>
+                        { subSection.title }
+                    </div>
+                    <div style={ {...styles.subSubSection.wrapper, ...(props.isOpened ? styles.subSubSection.opened: {})} }>
+                        { components }
+                    </div>
             </div>
         )
     } else {
         // Return sub section as link
-        return <div className="sub-section--link">{ subSection.title }</div>
+        return <div style={ styles.subSection.link }>{ subSection.title }</div>
     }
 }
 
@@ -67,16 +138,62 @@ const SidebarSection = props => {
         return components
     }
 
+    const unitHeight = constants.styles.unitHeight
+    const unitWidth = constants.styles.unitWidth
+
+    const styles = {
+        viewWrapper: {
+            overflow: 'hidden',
+            width: 'calc(3 * ' + unitWidth + ')',
+            transition: 'width 1s linear',
+        },
+        viewWrapperOpened: {
+            width: 'calc(6 * ' + unitWidth + ')',
+        },
+        section: {
+            wrapper: {
+                display: 'flex',
+                flexDirection: 'column',
+                position: 'relative',
+                height: 'calc(4 * ' + unitHeight + ')',
+                maxHeight: unitHeight,
+                overflow: 'visible',
+                transition: 'max-height 1s linear',
+            },
+            title: {
+                minHeight: unitHeight,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(255, 255, 255, 0.3)',
+                width: 'calc(3 * ' + unitWidth + ')',
+            },
+            opened: {
+                maxHeight: 'calc(4 * ' + unitHeight + ')',
+            }
+        },
+        subSections: {
+            wrapper: {
+                display: 'flex',
+                flexDirection: 'column',
+                position: 'relative',
+                width: 'calc(3 * ' + unitWidth + ')',
+            },
+        },
+    }
+
     return (
-        <div className={ "section--wrapper" + (props.isOpened ? " opened" : "") }>
+        <div style={ { ...styles.viewWrapper, ...(props.isOpened ? styles.viewWrapperOpened : {})} }>
+        <div style={ { ...styles.section.wrapper, ...(props.isOpened ? styles.section.opened : {})} }>
             <div
-                className="section--title"
+                style={ styles.section.title }
                 onClick={ () => props.dispatch.toggleSidebarSection(props.id.section) }>
                 { section.title }
             </div>
-            <div className="sub-sections--wrapper">
+            <div style={ styles.subSections.wrapper }>
                 { parseSection(section) }
             </div>
+        </div>
         </div>
     )
 }
@@ -101,11 +218,73 @@ const SidebarDumb = props => {
         return components
     }
 
+    const sidebarWidth =  constants.styles.sidebar.widthFactor + ' * ' + constants.styles.unitWidth
+
+    // Returns true if a subsetion is opened and the sidebar needs to be wider to show the subsections
+    const isSubSectionOpened = () => {
+        const { linkStates } = props
+
+        let i = 0
+
+        for(let j = 0; j < linkStates.length; j++)
+            for(let k = 0; k < linkStates[j].subSections.length; k++)
+                if(linkStates[j].subSections[k])
+                    i++
+
+        return i ? true : false
+    }
+
+    const styles = {
+        wrapper: {
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            height: '100vh',
+            display: 'flex',
+            flex: 1,
+            width: 'calc(' + sidebarWidth + ' * 2)',
+            maxWidth: 'calc(' + sidebarWidth + ' + 1px)', // 1px is for borderDiv border
+            paddingTop: '10rem',
+            transition: 'max-width 1s linear',
+            overflow: 'hidden',
+        },
+        logo: {
+            height: '10rem',
+            position: 'absolute',
+            width: 'calc(' + sidebarWidth + ' ' + '/ 1)',
+            top: 0,
+        },
+        sectionsWrapper: {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        opened: {
+        },
+        large: {
+            maxWidth: 'calc(' + sidebarWidth + ' * 2)',
+        },
+        borderDiv: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '100vh',
+            minWidth: 'calc(' + sidebarWidth + ')',
+            borderRight: '1px solid rgba(255, 255, 255, 0.6)',
+            pointerEvents: 'none',
+        }
+    }
+
     return (
-        <div className={ "sidebar--wrapper " + (props.isSiderbarOpened ? "" : " closed ") }>
-            <img src={ logo } alt="logo" className="logo" />
-            <div className="sections--wrapper">
+        <div style={ {
+                ...styles.wrapper,
+                ...(props.isSiderbarOpened ? styles.opened : {}),
+                ...(isSubSectionOpened() ? styles.large : {}),
+        } }>
+            <img src={ logo } alt="logo" style={ styles.logo } />
+            <div>
                 { parseSections(props.links) }
+            </div>
+            <div style={ styles.borderDiv }>
             </div>
         </div>
     )
