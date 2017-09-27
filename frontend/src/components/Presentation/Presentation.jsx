6b55612a95f9006c81a60aa12c91926aea232906
Thomas Binetruy <tbinetruy@gmail.com>
2017-09-27 12:29:49 -0400

    refactored bg split transition out of presentation component

--- frontend/src/components/Presentation/Presentation.jsx
+++ frontend/src/components/Presentation/Presentation.jsx
@@ -11,6 +11,118 @@ import {
     updateTransitionProgress
 } from '../../reducer/actions/Bg.js'
 
+import store from '../../store.js'
+const dispatch = store.dispatch
+
+const transitions = { splitBackground: {} }
+
+/*
+    Updates the backgrounds between slides prior to the slide transition
+    - param sign {number} positive for next slide and negative for previous slide
+    - pages {object[]} array of pages
+    - currentPage {number} index
+**/
+transitions.splitBackground.updateBackgroundUrls = (sign, pages, currentPage) => {
+    const totalPages = pages.length
+
+    const previousPage = currentPage - 1 < 0 ? 0 : currentPage - 1
+    const nextPage = currentPage + 1 > totalPages - 1 ? totalPages - 1 : currentPage + 1
+
+    const frontBgUrl = sign < 0 ? pages[currentPage].bgUrl : pages[nextPage].bgUrl
+    const backBgUrl = sign < 0 ? pages[previousPage].bgUrl : pages[currentPage].bgUrl
+
+    dispatch(updateFrontBgUrl(frontBgUrl))
+    dispatch(updateBackBgUrl(backBgUrl))
+}
+
+/*
+    Reset background styles so next transition appears smoothly
+    - param sign {number} positive for next slide and negative for previous slide
+**/
+transitions.splitBackground.resetBackgroundStyles = (sign) => {
+    if(sign > 0) {
+        dispatch(updateFrontBgStyle({ opacity: 0 }))
+        dispatch(updateTransitionProgress(0))
+    } else if (sign < 0) {
+        dispatch(updateFrontBgStyle({ opacity: 1 }))
+        dispatch(updateTransitionProgress(100))
+    }
+}
+
+/*
+    Split background slid
+    - param sign {number} positive for next slide and negative for previous slide
+    - pages {object[]} array of pages
+    - currentPage {number} index
+ transition
+**/
+transitions.splitBackground.slideTransition = (sign, pages, currentPage, attachScrollEvent, detachScrollEvent) => {
+    transitions.splitBackground.resetBackgroundStyles(sign)
+
+    // Upgrade backgrounds
+    transitions.splitBackground.updateBackgroundUrls(sign, pages, currentPage)
+
+    // Detach scroll event
+    detachScrollEvent()
+
+    // Get target page
+    const totalPages = pages.length
+
+    // boundary condition
+    const targetPage = sign > 0 ?
+                        (currentPage + 1 > totalPages - 1 ? totalPages - 1 : currentPage + 1)
+                        :
+                        currentPage
+
+    // boundary condition: don't animate backgrounds when going back on first page and going forward on last page
+    if((sign < 0 && currentPage <= 0) || (sign > 0 && currentPage >= totalPages - 1))
+        return attachScrollEvent()
+
+    // Animation handle
+    let transitionProgress = 0
+    let transitionTimer = 0
+    transitionTimer = window.setInterval(() => {
+        const condition = sign > 0 ? currentPage > targetPage : currentPage < targetPage
+
+        if(transitionProgress > 100) {
+            ////// Stop animation
+
+            // Attach scroll event to page change
+            attachScrollEvent()
+
+            // Clear interval
+            window.clearInterval(transitionTimer)
+            transitionTimer = undefined
+
+            // Border conditions
+            // if(currentPage < 0)
+                //this.setState({ currentPage: 0 })
+            //else if(currentPage > totalPages - 1)
+             //   this.setState({ currentPage: totalPages - 1 })
+
+            // update URL
+
+
+        } else {
+            ////// Continue scrolling
+
+            // Call background controls
+            if (sign > 0) {
+                dispatch(updateFrontBgStyle({ opacity: transitionProgress / 100 * 2 }))
+                dispatch(updateTransitionProgress(transitionProgress / 100 < 0.5 ? 0 :  (transitionProgress / 100 - 0.5) * 2))
+            } else {
+                dispatch(updateFrontBgStyle({ opacity: transitionProgress < 50 ? 1 : 1 - (transitionProgress / 100 - 0.5) * 2 }))
+                dispatch(updateTransitionProgress(1 - transitionProgress / 100 < 0.5 ? 0 :  (1 - transitionProgress / 100 - 0.5) * 2))
+            }
+
+            // Increment transition progress
+            transitionProgress = transitionProgress + 1
+        }
+    }, 5)
+}
+
+
+
 const mapStateToProps = state => ({
     routing: state.routing,
 })
@@ -24,6 +136,8 @@ const mapDispatchToProps = dispatch => ({
     goTo: url => dispatch(push(url)),
 })
 
+
+
 const PresentationDumb = props => {
     let currentPage = props.currentPage < 0 ? 0 : props.currentPage
     currentPage = currentPage > props.pages.length - 1 ? props.pages.length - 1 : currentPage
@@ -34,7 +148,6 @@ const PresentationDumb = props => {
     )
 }
 
-
 class Presentation extends React.Component {
     constructor(props) {
         super(props)
@@ -44,13 +157,10 @@ class Presentation extends React.Component {
         }
 
         this.onScroll = this.onScroll.bind(this)
-        this.updateBackgroundUrls = this.updateBackgroundUrls.bind(this)
-        this.splitBackgroundSlideTransition = this.splitBackgroundSlideTransition.bind(this)
         this.goToNextPage = this.goToNextPage.bind(this)
         this.goToPreviousPage = this.goToPreviousPage.bind(this)
         this.attachScrollEvent = this.attachScrollEvent.bind(this)
         this.detachScrollEvent = this.detachScrollEvent.bind(this)
-        this.resetBackgroundStyles = this.resetBackgroundStyles.bind(this)
     }
 
     componentDidMount() {
@@ -86,7 +196,7 @@ class Presentation extends React.Component {
         this.props.goToNextPage(currentPage)
 
         // Background transitions
-        this.splitBackgroundSlideTransition(1)
+        transitions.splitBackground.slideTransition(1, this.props.pages, currentPage, this.attachScrollEvent, this.detachScrollEvent)
 
         // Update state
         this.setState({ currentPage: nextPage })
@@ -107,7 +217,7 @@ class Presentation extends React.Component {
         this.props.goToPreviousPage(currentPage)
 
         // Background transitions
-        this.splitBackgroundSlideTransition(-1)
+        transitions.splitBackground.slideTransition(-1, this.props.pages, currentPage, this.attachScrollEvent, this.detachScrollEvent)
 
         // Update state
         this.setState({ currentPage: previousPage })
@@ -139,105 +249,6 @@ class Presentation extends React.Component {
             this.goToPreviousPage()
     }
 
-    /*
-       Updates the backgrounds between slides prior to the slide transition
-       - param sign {number} positive for next slide and negative for previous slide
-    **/
-    updateBackgroundUrls(sign) {
-        const { currentPage } = this.state
-        const totalPages = this.props.pages.length
-
-
-        const previousPage = currentPage - 1 < 0 ? 0 : currentPage - 1
-        const nextPage = currentPage + 1 > totalPages - 1 ? totalPages - 1 : currentPage + 1
-
-        const frontBgUrl = sign < 0 ? this.props.pages[currentPage].bgUrl : this.props.pages[nextPage].bgUrl
-        const backBgUrl = sign < 0 ? this.props.pages[previousPage].bgUrl : this.props.pages[currentPage].bgUrl
-
-        this.props.updateFrontBgUrl(frontBgUrl)
-        this.props.updateBackBgUrl(backBgUrl)
-    }
-
-    /*
-       Reset background styles so next transition appears smoothly
-    **/
-    resetBackgroundStyles(sign) {
-        if(sign > 0) {
-            this.props.updateFrontBgStyle({ opacity: 0 })
-            this.props.updateTransitionProgress(0)
-        } else if (sign < 0) {
-            this.props.updateFrontBgStyle({ opacity: 1 })
-            this.props.updateTransitionProgress(100)
-        }
-    }
-
-    /*
-       Split background slide transition
-    **/
-    splitBackgroundSlideTransition(sign) {
-        this.resetBackgroundStyles(sign)
-
-        // Upgrade backgrounds
-        this.updateBackgroundUrls(sign)
-
-        // Detach scroll event
-        this.detachScrollEvent()
-
-        // Get target page
-        const { currentPage } = this.state
-        const totalPages = this.props.pages.length
-
-        // boundary condition
-        const targetPage = sign > 0 ?
-                           (currentPage + 1 > totalPages - 1 ? totalPages - 1 : currentPage + 1)
-                         :
-                           currentPage
-
-        // boundary condition: don't animate backgrounds when going back on first page and going forward on last page
-        if((sign < 0 && currentPage <= 0) || (sign > 0 && currentPage >= totalPages - 1))
-            return this.attachScrollEvent()
-
-        // Animation handle
-        let transitionProgress = 0
-        this.transitionTimer = window.setInterval(() => {
-            const condition = sign > 0 ? currentPage > targetPage : currentPage < targetPage
-
-            if(transitionProgress > 100) {
-                ////// Stop animation
-
-                // Attach scroll event to page change
-                this.attachScrollEvent()
-
-                // Clear interval
-                window.clearInterval(this.transitionTimer)
-                this.transitionTimer = undefined
-
-                // Border conditions
-                if(this.state.currentPage < 0)
-                    this.setState({ currentPage: 0 })
-                else if(this.state.pages > totalPages - 1)
-                    this.setState({ currentPage: totalPages - 1 })
-
-                // update URL
-
-
-            } else {
-                ////// Continue scrolling
-
-                // Call background controls
-                if (sign > 0) {
-                    this.props.updateFrontBgStyle({ opacity: transitionProgress / 100 * 2 })
-                    this.props.updateTransitionProgress(transitionProgress / 100 < 0.5 ? 0 :  (transitionProgress / 100 - 0.5) * 2)
-                } else {
-                    this.props.updateFrontBgStyle({ opacity: transitionProgress < 50 ? 1 : 1 - (transitionProgress / 100 - 0.5) * 2 })
-                    this.props.updateTransitionProgress(1 - transitionProgress / 100 < 0.5 ? 0 :  (1 - transitionProgress / 100 - 0.5) * 2)
-                }
-
-                // Increment transition progress
-                transitionProgress = transitionProgress + 1
-            }
-        }, 5)
-    }
 
     render() {
 
@@ -253,6 +264,8 @@ class Presentation extends React.Component {
 Presentation.defaultProps = {
     goToNextPage: () => {},
     goToPreviousPage: () => {},
+    nextOnLastPage: () => console.log('next pres'),
+    previousOnFirstPage: () => console.log('prev pres'),
 }
 
 export default connect(
