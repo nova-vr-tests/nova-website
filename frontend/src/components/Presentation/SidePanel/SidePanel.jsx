cde8d7fe6bdf0875a94f958618eeb430d5baa440
Thomas Binetruy <tbinetruy@gmail.com>
2017-10-31 00:14:37 -0400

    current paragraph aligns with line on page change

--- frontend/src/components/Presentation/SidePanel/SidePanel.jsx
+++ frontend/src/components/Presentation/SidePanel/SidePanel.jsx
@@ -5,6 +5,8 @@ import { styles as appStyles } from '../../../constants.js'
 import * as React from 'react'
 import { connect } from 'react-redux'
 
+import { lifecycle, withState, compose } from 'recompose'
+
 import type { Page } from '../PresentationTypes.jsx'
 
 
@@ -120,9 +122,9 @@ const SidePanel: React.StatelessFunctionalComponent<Props> = props => {
             padding: 'calc(' + headerHeightCoef + ' * ' + appStyles.unitHeight + ') ' + appStyles.unitWidth,
         },
         slideParagraphs: {
-            //height: '70vh',
             height: 'calc(' + (24 - headerHeightCoef - titleHeightCoef - footerHeightCoef) + ' * ' + appStyles.unitHeight + ')',
-            overflowY: 'scroll',
+            overflowY: 'hidden',
+            overflowX: 'hidden',
         },
         head: {
             height: 'calc(' + headHeightCoef + ' * ' + appStyles.unitHeight + ') ',
@@ -137,11 +139,20 @@ const SidePanel: React.StatelessFunctionalComponent<Props> = props => {
             padding: 0,
             margin: 0,
         },
+        paragraph: {
+            height: 'calc(' + lineHeightCoef + ' * ' + appStyles.unitHeight + ') ',
+            display: 'flex',
+            alignItems: 'center',
+            width: '100%',
+        },
     }
 
     const { pid } = props.pages[props.currentPage]
-    const presSlides = props.pages.filter(e => e.pid === pid)
-    const content = presSlides.map(e => e.comp).map((Comp, i) => <Comp key={ i } />)
+    const presSlides = props.pages.map((e, i) => ({ ...e, i })).filter(e => e.pid === pid)
+    const paragraphsAboveLine = presSlides.filter(e => e.i < props.currentPage)
+    const paragraphsRest = presSlides.filter(e => e.i >= props.currentPage)
+    const contentAboveLine = paragraphsAboveLine.map((e, i) => <div key={ i } style={ e.i === props.currentPage ? styles.paragraph : {} }><e.comp key={ i } /></div>)
+    const contentRest = paragraphsRest.map((e, i) => <div key={ i } style={ e.i === props.currentPage ? styles.paragraph : {} }><e.comp key={ i } /></div>)
 
     return (
         <div style={ styles.wrapper }>
@@ -150,11 +161,16 @@ const SidePanel: React.StatelessFunctionalComponent<Props> = props => {
             />
             <div style={ styles.contentWrapper }>
                 <h2 style={ styles.title }>{ props.pages[props.currentPage].h2 }</h2>
-                <div style={ styles.slideParagraphs }>
+                <div style={ styles.slideParagraphs } id="side-panel-scroll">
                     <div>
                         <div style={ styles.head }>
                         </div>
-                        { content }
+                        <div id="paragraphs-top">
+                            { contentAboveLine }
+                        </div>
+                        <div>
+                            { contentRest }
+                        </div>
                         <div style={ styles.tail }>
                         </div>
                     </div>
@@ -164,9 +180,43 @@ const SidePanel: React.StatelessFunctionalComponent<Props> = props => {
     )
 }
 
+const scrollTo = (id, initScroll, targetScroll, progress, setScroll) => {
+    const el = document.getElementById(id)
+
+    const scrollDistance = targetScroll - initScroll
+    const currentScroll = initScroll + scrollDistance * progress
+    if(progress >= 1) {
+        return
+    } else {
+        el.scrollTo(0, currentScroll)
+        requestAnimationFrame(() => scrollTo(id, initScroll, targetScroll, progress + 0.05, setScroll))
+    }
+
+}
+
+const sidePanelLifecycle = {
+    componentDidMount: function() { this.setState({ scrollOffset: 0 }) },
+    componentDidUpdate: function(prevProps) {
+        const targetScroll = document.getElementById('paragraphs-top').clientHeight
+        const currentScroll = document.getElementById('side-panel-scroll').scrollTop
+
+        if(this.state.currentPage === prevProps.currentPage || this.state.scrollOffset !== 0) {
+            return
+        }
+
+        scrollTo('side-panel-scroll', currentScroll, targetScroll, 0, this.props.setScrollOffset)
+
+    },
+}
+
+const SidePanelSmart = compose(
+    withState('scrollOffset', 'setScrollOffset', 0),
+    lifecycle(sidePanelLifecycle),
+)(SidePanel)
+
 const ConnectedSidePanel = connect(
     mapStateToProps,
     mapDispatchToProps
-)(SidePanel)
+)(SidePanelSmart)
 
 export default ConnectedSidePanel
