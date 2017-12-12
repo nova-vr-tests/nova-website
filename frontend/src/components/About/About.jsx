b3c0f4a45d745a51231bd45bcef3af1f144579f8
Thomas Binetruy <tbinetruy@gmail.com>
2017-12-12 12:00:50 +0100

    added footer pages content

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -23,27 +23,61 @@ const mapStateToProps: MapStateToProps<ReduxState> = function(state) {
 
 const text = {}
 
+const legalsContent = (
+    <div>
+        <h2>Terms and conditions of use</h2>
+        <p>
+            We maintain this site as a resource and communication portal. Terms of Service apply to anyone browsing the website. Accessing the site is acceptance without limitation or qualification of the following Terms and Conditions. If you do NOT accept the Terms and Conditions, please discontinue your use of this site.
+        </p>
+        <h2>Privacy Policy</h2>
+        <p>
+            We use basic Google Analytics to improve our services. Opt-out of Google Analytics: https://tools.google.com/dl-page/gaoptout/
+        </p>
+    </div>
+)
+
+const contactContent = (
+    <div>
+        <p>
+            Thank you for your interest in Nova XR Media. Please email us with any question:
+        </p>
+        <strong><a href="mailto: joe@novamedia.nyc">joe@novamedia.nyc</a></strong>
+    </div>
+)
+
+const loginContent = (
+    <div>
+        <h2>Coming Soon</h2>
+    </div>
+)
+
+
 text[footerPage.LEGALS] = {
     title: 'legals',
+    content: legalsContent
 }
 
 text[footerPage.CONTACT] = {
     title: 'contact',
+    content: contactContent
 }
 
 text[footerPage.LOGIN] = {
     title: 'login',
+    content: loginContent
 }
 
 const AboutUs: React.StatelessFunctionalComponent<Props> = (props) => {
     const styles = getStyles(props)
 
     const title = text[props.currentFooterPage].title
+    const content = text[props.currentFooterPage].content
 
     return (
         <div style={ styles.wrapper }>
+            <h2>{ title }</h2>
             <SidePanel type={ sidePanelTypes.INVERTED }>
-                { title }
+                { content }
             </SidePanel>
         </div>
     )
