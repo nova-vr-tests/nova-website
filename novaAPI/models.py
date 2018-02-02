45676917292cf5fb90cfac78d8e1c7ba4cc596e1
Thomas Binetruy <tbinetruy@gmail.com>
2018-02-02 03:02:53 -0500

    stashing

--- novaAPI/models.py
+++ novaAPI/models.py
@@ -8,4 +8,9 @@ class BusinessProposition(models.Model):
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
+    specifications = models.FileField(upload_to='blog_pics')
+    content = MarkdownxField()
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
     content = MarkdownxField()
