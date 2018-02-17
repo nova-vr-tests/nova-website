949fd7c26e026b0460fd7f9485203f90b0073820
Thomas Binetruy <tbinetruy@gmail.com>
2018-02-16 23:47:32 -0500

    added a bunch of tests for Presentation

--- frontend/src/components/Presentation/__tests__/Presentation.test.jsx
+++ frontend/src/components/Presentation/__tests__/Presentation.test.jsx
@@ -1,5 +1,6 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import _ from 'lodash'
 
 import {
     PresentationDumb,
@@ -13,9 +14,142 @@ import SlideTransition from '../SlideTransition/SlideTransition.jsx'
 
 import { sidePanelTypes } from '../SidePanel/SidePanel.jsx'
 
+const reduxDispatchProps = {
+    updateCurrentPage: jest.fn(),
+    updateBackLayers: jest.fn(),
+    updateAppTheme: jest.fn(),
+    updateTransitionProgress: jest.fn(),
+    goTo: jest.fn(),
+    updateLinePosition: jest.fn(),
+    updateGoToPage: jest.fn(),
+    updatePages: jest.fn(),
+    updateMainPanelIsOpened: jest.fn(),
+    updateMainPanelContent: jest.fn(),
+    updateSidePanelHeaderOverride: jest.fn(),
+}
+
+const reduxStateProps = {
+    pathname: '/',
+    pages: [{}],
+    currentPage: 0,
+}
+
+const initProps = {
+    ...reduxDispatchProps,
+    ...reduxStateProps,
+}
+
+const presentationSmartFactory = (mockKeys, props = initProps) => {
+    const subject = new PresentationSmart(_.cloneDeep(props))
+
+    for(let key in mockKeys) {
+        subject[mockKeys[key]] = jest.fn()
+    }
+
+    return subject
+}
+
+const initGlobalMocks = () => {
+}
+
 describe('PresentationSmart', () => {
+    beforeEach(() => {
+        initGlobalMocks()
+    })
+
     test('componentDidMount', () => {
-        expect(1).toEqual(1)
+        const subject = presentationSmartFactory(['attachScrollEvent', 'updateLinePosition'])
+        subject.componentDidMount()
+
+        expect(subject.props.updatePages.mock.calls).toEqual([[subject.props.pages]])
+        expect(subject.attachScrollEvent.mock.calls).toEqual([[]])
+        expect(subject.updateLinePosition.mock.calls).toEqual([[]])
+    })
+
+    test('componentWillUnmount', () => {
+        const subject = presentationSmartFactory(['detachScrollEvent'])
+
+        subject.componentWillUnmount()
+        expect(subject.detachScrollEvent.mock.calls).toEqual([[]])
+    })
+
+    test('componentWillUpdate', () => {
+        const subject = presentationSmartFactory(['updateAppTheme'])
+
+        let isFooterOpened = false
+        let nextProps = { isFooterOpened, currentPage: 10 }
+        subject.componentWillUpdate(nextProps)
+        expect(subject.updateAppTheme.mock.calls).toEqual([[nextProps.currentPage]])
+
+        isFooterOpened = true
+        nextProps = { isFooterOpened, currentPage: 10 }
+        subject.componentWillUpdate(nextProps)
+        expect(subject.updateAppTheme.mock.calls[1]).toEqual(undefined)
+    })
+
+    test('componentDidUpdate', () => {
+        const subject = presentationSmartFactory(['resetScrollEvent'])
+
+        subject.state = { ...subject.state, scrollEvent: true }
+        subject.componentDidUpdate()
+        expect(subject.resetScrollEvent.mock.calls).toEqual([[]])
+
+        subject.state = { ...subject.state, scrollEvent: false }
+        subject.componentDidUpdate()
+        expect(subject.resetScrollEvent.mock.calls[1]).toEqual(undefined)
+    })
+
+    test('resetScrollEvent', () => {
+        const subject = presentationSmartFactory(['setState'])
+
+        subject.resetScrollEvent()
+        expect(subject.setState.mock.calls).toEqual([[{ scrollEvent: null }]])
+    })
+
+    test('updateAppTheme', () => {
+        const subject = presentationSmartFactory(['setState'])
+        subject.props.updateAppTheme = jest.fn()
+        subject.props.pages.push({ theme: 'foo' })
+
+        const currentPage = 1
+        subject.updateAppTheme(currentPage)
+        expect(subject.props.updateAppTheme.mock.calls).toEqual([[subject.props.pages[currentPage].theme]])
+    })
+
+    test('pathnameToSlideNumber', () => {
+        const subject = presentationSmartFactory(['setState'])
+
+        let returnValue = subject.pathnameToSlideNumber('/')
+        expect(returnValue).toEqual(0)
+
+        const path1 = '/foo'
+        const path2 = '/bar'
+        subject.props.pages[0].path = path1
+        subject.props.pages.push({ path: path2})
+
+        returnValue = subject.pathnameToSlideNumber(path2)
+        expect(returnValue).toEqual(1)
+        returnValue = subject.pathnameToSlideNumber(path1)
+        expect(returnValue).toEqual(0)
+    })
+
+    test('componentWillReceiveProps', () => {
+        const subject = presentationSmartFactory(['updateSlideFromUrl', 'updateLinePosition','updateMainPanel', 'updateSlideHeaderOverride'])
+
+        const nextProps = {
+            pathname: '/foo',
+            currentPage: 0,
+        }
+
+        subject.componentWillReceiveProps(nextProps)
+        expect(subject.updateSlideFromUrl.mock.calls).toEqual([[nextProps.pathname]])
+        expect(subject.updateLinePosition.mock.calls).toEqual([[nextProps]])
+        expect(subject.updateMainPanel.mock.calls).toEqual([])
+        expect(subject.updateSlideHeaderOverride.mock.calls).toEqual([[nextProps]])
+
+        nextProps.currentPage = 1
+        subject.componentWillReceiveProps(nextProps)
+        expect(subject.updateMainPanel.mock.calls).toEqual([[nextProps]])
     })
 })
 
@@ -69,8 +203,5 @@ describe('PresentationDumb', () => {
 
         // SlideTransition
         expect(subject.find(SlideTransition).props()).toEqual(_props.SlideTransition)
-    })
-
-    test('mounts with correct styles', () => {
     })
 })
