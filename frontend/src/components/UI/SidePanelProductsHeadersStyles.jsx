b86f4fb38f6ee66abb0ef2d26121daf39b810bf2
Thomas Binetruy <tbinetruy@gmail.com>
2018-04-18 13:04:48 -0400

    new file

---
+++ frontend/src/components/UI/SidePanelProductsHeadersStyles.jsx
@@ -0,0 +1,15 @@
+import { styles as appStyles } from '../../constants'
+
+const getStyles = props => {
+    const { clientWidth } = document.documentElement
+
+    if(clientWidth < appStyles.mediaQueries.phone) {
+    }
+
+    const styles = {
+    }
+
+    return styles
+}
+
+export default getStyles
