f7ce8551f83eac6a91f2e798a93e18985934db0c
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-10-13 21:51:41 +0200

    added ajgno heroku

--- novawebsite/settings.py
+++ novawebsite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 import datetime
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -193,3 +195,6 @@ expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
 MARKDOWNX_MARKDOWN_EXTENSIONS = [
     'markdown.extensions.tables'
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
