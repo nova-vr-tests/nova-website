177836b919910e0cb2e6d44b93fcbf44a61a6f10
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-11-01 21:07:25 +0100

    refactored page models

--- novaAPI/admin.py
+++ novaAPI/admin.py
@@ -3,6 +3,22 @@ from markdownx.admin import MarkdownxModelAdmin
 from .models import *
 from django.utils.html import format_html
 
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ['title']
+    search_fields = ['title']
+
+class SubsectionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ['title']
+    search_fields = ['title']
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ['title']
+    search_fields = ['title']
+
 class BusinessPropositionAdmin(admin.ModelAdmin):
     list_display = ('title', 'url', 'picto_img', 'bg_img', 'password', 'date', 'pdf_link', 'storage_link')
     list_filter = ['date']
@@ -26,11 +42,6 @@ class BusinessPropositionAdmin(admin.ModelAdmin):
 
         return format_html("<a target='_blank' href='{0}'>Dropbox link</a>", obj.storage.split('?')[0])
 
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'date')
-    list_filter = ['date', 'author']
-    search_fields = ['title', 'content']
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'url', 'picto_img', 'bg_img', 'square_picto_img', 'date')
     list_filter = ['date']
@@ -38,7 +49,6 @@ class ProductAdmin(admin.ModelAdmin):
 
     def url(self, obj):
         return format_html("<a target='_blank' href='/products?post={0}'>Link</a>", obj.id)
-
 
     def picto_img(self, obj):
         pictoBgUrl = ''
@@ -71,104 +81,15 @@ class ProductAdmin(admin.ModelAdmin):
         return format_html("<a href='{0}' target='_blank'><img src='{0}' alt='picto' style='height: 5rem; width: 10rem;' /></a>", obj.bg_image.url.split('?')[0])
 
 
-
-class PublicationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-
-
-class IndustryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class CrossIndustryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class LeaningLabAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class AboutUsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class CommunityAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class DesignAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class ProgramAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class ProduceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class NetworkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class DeployAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-
-class ProductionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class PartnerAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
-class CareerAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
-    list_filter = ['date']
-    search_fields = ['title', 'content']
-
 class BuildXRAdmin(admin.ModelAdmin):
     list_display = ('date',)
     list_filter = ['date']
     search_fields = ['json']
-
 # Register your models here.
 admin.site.register(BusinessProposition, BusinessPropositionAdmin)
-admin.site.register(BlogPost, BlogPostAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Industry, IndustryAdmin)
-admin.site.register(CrossIndustry, CrossIndustryAdmin)
-admin.site.register(LearningLab, LeaningLabAdmin)
-admin.site.register(AboutUs, AboutUsAdmin)
-admin.site.register(Publication, PublicationAdmin)
-
-admin.site.register(Community, CommunityAdmin)
-admin.site.register(Design, DesignAdmin)
-admin.site.register(Program, ProgramAdmin)
-admin.site.register(Produce, ProduceAdmin)
-admin.site.register(Network, NetworkAdmin)
-admin.site.register(Deploy, DeployAdmin)
-
-admin.site.register(Production, ProduceAdmin)
-admin.site.register(Partner, PartnerAdmin)
-admin.site.register(Career, CareerAdmin)
 admin.site.register(BuildXR, BuildXRAdmin)
 
+admin.site.register(Section, SectionAdmin)
+admin.site.register(Subsection, SubsectionAdmin)
+admin.site.register(Page, PageAdmin)
--- novaAPI/models.py
+++ novaAPI/models.py
@@ -8,6 +8,34 @@ from markdownx.models import MarkdownxField
 
 from django.contrib.auth.models import User
 
+
+
+class Section(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+
+    def __str__(self):
+        return self.title
+
+class Subsection(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    introduction = MarkdownxField()
+    content_text = MarkdownxField()
+
+    def __str__(self):
+        return self.title
+
+class Page(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    subsection = models.ForeignKey(Subsection, on_delete=models.CASCADE)
+    date = models.DateTimeField('date published', default=datetime.now)
+    picto = models.ImageField(upload_to='imgs')
+    content = MarkdownxField()
+
+    def __str__(self):
+        return self.title
+
+
 class BuildXR(models.Model):
     json = models.TextField()
     date = models.DateTimeField('Submission date', default=datetime.now)
@@ -31,16 +59,6 @@ class BusinessProposition(models.Model):
         verbose_name = 'Business Proposition'
         verbose_name_plural = 'Business Propositions'
 
-class BlogPost(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    author = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
-    picto = models.ImageField(upload_to='blog_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Blog Post'
-        verbose_name_plural = 'Lab Live'
 
 class Product(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
@@ -57,145 +75,3 @@ class Product(models.Model):
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
 
-
-class Publication(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='publication_pics')
-    pdf = models.FileField(upload_to='publication_pdfs')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Publication'
-        verbose_name_plural = 'Publications'
-
-
-class Industry(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='industry_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Industry'
-        verbose_name_plural = 'Industries'
-
-class CrossIndustry(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='cross_industry_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Cross Industry'
-        verbose_name_plural = 'Cross Industry'
-
-class LearningLab(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='learning_lab_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Learning Lab'
-        verbose_name_plural = 'Learning Lab'
-
-class AboutUs(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='about_us_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'About Us'
-        verbose_name_plural = 'About Us'
-
-class Community(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='community_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Community'
-        verbose_name_plural = 'Communities'
-
-class Design(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='design_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Design'
-        verbose_name_plural = 'Design'
-
-class Program(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='program_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Program'
-        verbose_name_plural = 'Program'
-
-class Produce(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='produce_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Produce'
-        verbose_name_plural = 'Produce'
-
-class Network(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='network_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Network'
-        verbose_name_plural = 'Network'
-
-class Deploy(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='deploy_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Deploy'
-        verbose_name_plural = 'Deploy'
-
-class Production(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='productions_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Production'
-        verbose_name_plural = 'Productions'
-
-class Partner(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='partners_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Partner'
-        verbose_name_plural = 'Partners'
-
-class Career(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
-    date = models.DateTimeField('date published', default=datetime.now)
-    picto = models.ImageField(upload_to='careers_pics')
-    content = MarkdownxField()
-
-    class Meta:
-        verbose_name = 'Career'
-        verbose_name_plural = 'Careers'
